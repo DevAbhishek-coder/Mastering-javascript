f52b9523e81ab7f88f814b1b7f95304f6302e093
DevAbhishek-coder <sabhi51552@gmail.com>
2025-01-27 08:34:07 +0000

    conversion of datatype

---
+++ 01_basic/03_datatype coversionoperatio.js
@@ -0,0 +1,25 @@
+// let score = "33"
+
+// console.log(typeof score)
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// if in number we add write some string value so it will show "NaN"
+// for null it will convert data into 0
+// for undefined value it will show Nan
+// for string value it will not convert into number it shows NaN
+// "33"=> 33
+// "33abs"=> Nan
+
+// let isLoggedIn = 1
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+// for boolean 1 => will convert in true and 0=> will convert into false
+
+let somenumber = 33
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
