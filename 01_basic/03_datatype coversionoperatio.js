7f46016ca6c60a5b5b47d70a1013477fa88b6cae
DevAbhishek-coder <sabhi51552@gmail.com>
2025-01-27 09:09:23 +0000

    conversion

--- 01_basic/03_datatype coversionoperatio.js
+++ 01_basic/03_datatype coversionoperatio.js
@@ -23,3 +23,42 @@ let somenumber = 33
 let stringNumber = String(somenumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// Operations
+
+let number = 3
+let negvalue = -number
+console.log(negvalue)
+
+// Here we have convert a positive value in to the negative value by using a operation.
+// Some basic opeartion which we also use are.
+
+// console.log(2+2)- It will sum the number
+// console.log(2-2)- it will subtract the number
+// console.log(2*2)-It will multiply the number
+// console.log(2**2)- It will give power like 2 the 3
+// console.log(2/2)- It will divide the number
+// console.log(2%2)- It will show remainder
+
+let str1 = "Hello"
+let str2 = " Abhishek"
+
+let str3 = str1 + str2 
+console.log(str3)
+
+// Here we have add the two variables
+
+console.log(1 +"2")
+console.log("1" + 2)
+console.log("1" + "2")
+console.log(1+2+"3")
+
+// What happening here is that if the string value is first javascripe will take all value into the string form same if number is first it will took all value in to number.
+
+//       Some special or tricky converions
+
+console.log(+true)
+console.log(+"")
+
+// These are some tricky conversion but it is not a good practice
