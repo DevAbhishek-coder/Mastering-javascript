bb09715ff2e6e43e46f011fd5c426b8828b113e7
DevAbhishek-coder <sabhi51552@gmail.com>
2025-01-27 09:50:31 +0000

    datatype summary

---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,35 @@
+// 1.In JS data are catoriges in to two type
+
+// i-Primitive (Callback)
+// There are 7 - String,Number,boolean,null,undefined,Symbol,Bigint
+// String= "Hello"
+// Number = 3
+// Boolean = true or false
+// null = Empty
+// undefined= value is not define
+
+// sumbole = symbole(1'2'3)
+// Bigint are big number= 456515615656156n
+
+
+
+
+
+// ii-Reference or Non-Primitve
+// Array,objects,Functions
+
+// const heros = ["Shaktiman","naga"]- Arrays are always written into a square bracket. Objects are always into a square bracket
+
+// {
+//     name: "Hitesh",
+//     Age: 32.
+// }
+
+// Object are always written into a currly bracket.values inside currly bracket are always objects
+
+
+
+
+// JS is a dynamic language
+
+
