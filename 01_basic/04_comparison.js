a66d55c3bd495b21a3afa8ff70fc3e1d124abb32
DevAbhishek-coder <sabhi51552@gmail.com>
2025-01-27 09:27:27 +0000

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,26 @@
+// Comparison are use compare two or more values
+// Example like:-
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2=1)
+// These are some basic comparison operator. And answer would be also be in a boolean form like true or false
+
+// Comparison between two datatypes
+
+// console.log("1">2)
+// console.log("01">2)
+// Here we  have compare two datatypes. Which show false.Becasue JS has convert string datatype into number automatticaly
+
+// Note:- In Js Comparison and Equality have different way to work
+
+// Let compare between null and number datatype
+
+console.log(null > 0);
+console.log(null == 0 );
+console.log(null >=0 );
+
+// null will auto change into 0.
+// Do not pratice these comparison should avoid.
+// Always use clean code
